c0c76ca2f5f72dcaefa410e73f6af713932a31ac
harsh <harshitchandel318@gmail.com>
2026-04-18 20:44:23 +0530

    Add LoginTest with DDT and POJO assertions

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,31 @@
+package tests;
+
+import dataproviders.LoginDataProvider;
+import models.LoginData;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+
+public class LoginTest extends BaseTest {
+
+    @Test(dataProvider = "loginData", dataProviderClass = LoginDataProvider.class)
+    public void testLogin(LoginData testData) {
+
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login(testData.username, testData.password);
+
+        if (testData.expectedResult.equalsIgnoreCase("success")) {
+            Assert.assertTrue(
+                    loginPage.isLoginSuccessful(),
+                    "Expected success but failed for: " + testData.username
+            );
+        } else {
+            Assert.assertFalse(
+                    loginPage.isLoginSuccessful(),
+                    "Expected failure but passed for: " + testData.username
+            );
+        }
+    }
+}
